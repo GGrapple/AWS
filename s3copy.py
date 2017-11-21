b750cd776a7e36481129d0a4d53e000be0e5f098
Dan <danial.white@gmail.com>
2017-11-21 16:23:48 -0500

    Added parser for keys, store them in Keys.ini

--- s3copy.py
+++ s3copy.py
@@ -6,6 +6,26 @@ import sys
 import os
 import json
 import boto3
+import configparser
+
+'''
+Setup Parser
+'''
+Config = configparser.ConfigParser()
+Config.read('keys.ini')
+Config.sections()
+def ConfigSectionMap(section):
+    dict1 = {}
+    options = Config.options(section)
+    for option in options:
+        try:
+            dict1[option] = Config.get(section, option)
+            if dict1[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
 
 '''
 need env variable below pointing to credentials
@@ -16,8 +36,8 @@ os.environ['AWS_SHARED_CREDENTIALS_FILE'] = '~/.aws/credentials'
 session = boto3.Session(profile_name='saml')
 boto3.setup_default_session(profile_name='saml')
 
-dest_bucket_name = ''
-kms_key = ''
+dest_bucket_name = ConfigSectionMap('Bucket')['bucket_name']
+kms_key = ConfigSectionMap('Keys')['kms_id']
 file_key = sys.argv[1]
 dest_bucket_region = 'us-gov-west-1'
 
